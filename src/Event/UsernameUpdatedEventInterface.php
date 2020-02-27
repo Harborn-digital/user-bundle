11e958b57002b481cbe2e29061bb339f8bd1f4ff
Jaimy Broere <jaimy@connectholland.nl>
2020-02-27 13:27:55 +0100

    Add Interface for UsernameUpdatedEvent to make testing possible

--- src/Event/UsernameUpdatedEventInterface.php
+++ src/Event/UsernameUpdatedEventInterface.php
@@ -9,8 +9,6 @@ declare(strict_types=1);
 
 namespace ConnectHolland\UserBundle\Event;
 
-use Symfony\Component\EventDispatcher\GenericEvent;
-
-final class UsernameUpdatedEvent extends GenericEvent implements UsernameUpdatedEventInterface
+interface UsernameUpdatedEventInterface
 {
 }
